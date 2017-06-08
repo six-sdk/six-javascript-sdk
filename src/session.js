5b06687a75f480a9a7a64256c0a55b3c2eb9c5ab
Johan Ipsonius <Johan.Ipsonius@six-group.se>
2017-06-08 11:50:23 +0200

    Converted all Object.values, Object.keys and Array.forEach to for and for in loops
    The polyfill for Object.values had performance issues in Microsoft Edge. Object.keys and Array.forEach also don't have stellar performance. Therefore we decided to switch all of these to for loops instead.

--- src/session.js
+++ src/session.js
@@ -23,9 +23,10 @@ export default function (token, endpoint) {
   setInterval(() => {
     // TODO: we can most likely do something more sofisticated here be timing the loop
     // and only publish X items per frame
-    const items = Object.values(queue)
-    for (let i = 0; i < items.length; i++) {
-      items[i].call()
+    for (let item in queue) {
+      if (queue.hasOwnProperty(item) && queue.propertyIsEnumerable(item)) {
+        queue[item].call()
+      }
     }
     queue = {}
   }, batchInterval)
@@ -61,27 +62,38 @@ export default function (token, endpoint) {
           const sessionExpiredSubscriptions = resourceToSubscription['session-expired']
           if (sessionExpiredSubscriptions) {
             const called = Object.create(null)
-            sessionExpiredSubscriptions.forEach(s => {
-              if (!called[s.id]) {
-                s.callback(err, data, s.unsubscribeFn)
-                called[s.id] = true
+            for (let i in sessionExpiredSubscriptions) {
+              if (sessionExpiredSubscriptions.hasOwnProperty(i)) {
+                const s = sessionExpiredSubscriptions[i]
+                if (!called[s.id]) {
+                  s.callback(err, data, s.unsubscribeFn)
+                  called[s.id] = true
+                }
               }
-            })
+            }
           }
         }
         const errorSubscriptions = resourceToSubscription['error']
         if (errorSubscriptions) {
           const called = Object.create(null)
-          errorSubscriptions.forEach(s => {
-            if (!called[s.id]) {
-              setTimeout(() => { s.callback(err, data, s.unsubscribeFn) })
-              called[s.id] = true
+          for (let i in errorSubscriptions) {
+            if (errorSubscriptions.hasOwnProperty(i)) {
+              const s = errorSubscriptions[i]
+              if (!called[s.id]) {
+                setTimeout(() => { s.callback(err, data, s.unsubscribeFn) })
+                called[s.id] = true
+              }
             }
-          })
+          }
         }
         let subscriptions = resourceToSubscription[resource]
         if (subscriptions) {
-          subscriptions.forEach(s => s.callback(err, data, s.unsubscribeFn))
+          for (let i in subscriptions) {
+            if (subscriptions.hasOwnProperty(i)) {
+              const s = subscriptions[i]
+              s.callback(err, data, s.unsubscribeFn)
+            }
+          }
         }
       },
 
@@ -111,62 +123,70 @@ export default function (token, endpoint) {
 
         // find all entities (recursivly) in the resource we publishing
         const entities = findAllEntitiesIn(data)
+        const resourcesToNotify = Object.create(null) // in lieu of Set
 
-        // merge entities into cache
-        Object.values(entities).forEach(entity => {
-          if (entity.url === data.url) {
-            entityCache[entity.url] = deepMerge(entityCache[entity.url], entity, replace)
-          } else {
-            entityCache[entity.url] = deepMerge(entityCache[entity.url], entity)
-          }
-        })
+        for (let entityIndex in entities) {
+          if (entities.hasOwnProperty(entityIndex) && entities.propertyIsEnumerable(entityIndex)) {
+            const entity = entities[entityIndex]
+            // merge entities into cache
+            if (entity.url === data.url) {
+              entityCache[entity.url] = deepMerge(entityCache[entity.url], entity, replace)
+            } else {
+              entityCache[entity.url] = deepMerge(entityCache[entity.url], entity)
+            }
 
-        // merge cache into entities
-        Object.values(entities).forEach(entity => {
-          Object.assign(entity, entityCache[entity.url])
-        })
+            // merge cache into entities
+            Object.assign(entity, entityCache[entity.url])
 
-        // resolve all entity references, with entity object instances from the cache
-        Object.values(entities).forEach(entity => {
-          for (var field in entity) {
-            if (entity.hasOwnProperty(field)) {
-              if (entity[field] && typeof entity[field] === 'object') {
-                if (entity[field].url) {
-                  entity[field] = entityCache[entity[field].url] || entity[field]
+            // resolve all entity references, with entity object instances from the cache
+            for (let field in entity) {
+              if (entity.hasOwnProperty(field)) {
+                if (entity[field] && typeof entity[field] === 'object') {
+                  if (entity[field].url) {
+                    entity[field] = entityCache[entity[field].url] || entity[field]
+                  }
                 }
               }
             }
+
+            // entityToResource mapping
+            entityToResource[entity.url] = entityToResource[entity.url] || {}
+            entityToResource[entity.url][resource] = true
+
+            // fetch all resources to notify subscribers for
+            if (entityToResource[entity.url]) {
+              for (let r in entityToResource[entity.url]) {
+                resourcesToNotify[r] = true
+              }
+            }
           }
-        })
+        }
 
         // Domain specific, fixup level 1 in all cached listings with orderbooks
-        Object.values(entityCache).forEach(e => {
-          // check if this entity have quotes and orderbook
-          if (e.orderbook && e.orderbook.lastUpdated && e.orderbook.levels && e.orderbook.levels.length > 0) {
-            if (e.quotes && e.quotes.lastUpdated) {
-              if (e.orderbook.levels[0].bidPrice !== e.quotes.bidPrice || e.orderbook.levels[0].askPrice !== e.quotes.askPrice) {
-                // which of the orderbook and quotes have the latest lastUpdated
-                if (new Date(e.orderbook.lastUpdated).getTime() > new Date(e.quotes.lastUpdated).getTime()) {
-                  // copy orderbook to quotes
-                  e.quotes.bidPrice = e.orderbook.levels[0].bidPrice
-                  e.quotes.askPrice = e.orderbook.levels[0].askPrice
-                  e.quotes.lastUpdated = e.orderbook.lastUpdated
-                } else {
-                  // copy quotes to orderbook
-                  e.orderbook.levels[0].bidPrice = e.quotes.bidPrice
-                  e.orderbook.levels[0].askPrice = e.quotes.askPrice
-                  e.orderbook.lastUpdated = e.quotes.lastUpdated
+        for (let entityCacheIndex in entityCache) {
+          if (entityCache.hasOwnProperty(entityCacheIndex) && entityCache.propertyIsEnumerable(entityCacheIndex)) {
+            const e = entityCache[entityCacheIndex]
+            // check if this entity have quotes and orderbook
+            if (e.orderbook && e.orderbook.lastUpdated && e.orderbook.levels && e.orderbook.levels.length > 0) {
+              if (e.quotes && e.quotes.lastUpdated) {
+                if (e.orderbook.levels[0].bidPrice !== e.quotes.bidPrice || e.orderbook.levels[0].askPrice !== e.quotes.askPrice) {
+                  // which of the orderbook and quotes have the latest lastUpdated
+                  if (new Date(e.orderbook.lastUpdated).getTime() > new Date(e.quotes.lastUpdated).getTime()) {
+                    // copy orderbook to quotes
+                    e.quotes.bidPrice = e.orderbook.levels[0].bidPrice
+                    e.quotes.askPrice = e.orderbook.levels[0].askPrice
+                    e.quotes.lastUpdated = e.orderbook.lastUpdated
+                  } else {
+                    // copy quotes to orderbook
+                    e.orderbook.levels[0].bidPrice = e.quotes.bidPrice
+                    e.orderbook.levels[0].askPrice = e.quotes.askPrice
+                    e.orderbook.lastUpdated = e.quotes.lastUpdated
+                  }
                 }
               }
             }
           }
-        })
-
-        // entityToResource mapping
-        Object.values(entities).forEach(entity => {
-          entityToResource[entity.url] = entityToResource[entity.url] || {}
-          entityToResource[entity.url][resource] = true
-        })
+        }
 
         // update the resourceCache not all resources are entities,
         // for them we just merge into the resourceCache
@@ -177,41 +197,35 @@ export default function (token, endpoint) {
         }
 
         // fixup all references from resourceCache to entityCache
-        Object.keys(resourceCache).forEach(key => {
-          if (resourceCache[key].url) {
+        for (let key in resourceCache) {
+          if (resourceCache.hasOwnProperty(key) && resourceCache[key].url) {
             const cachedEntity = entityCache[resourceCache[key].url]
             if (cachedEntity) {
               resourceCache[key] = cachedEntity
             }
           }
-        })
-
-        // fetch all resources to notify subscribers for
-        const resourcesToNotify = Object.create(null) // in lieu of Set
-        Object.values(entities).forEach(entity => {
-          Object.keys(entityToResource[entity.url]).forEach(r => (resourcesToNotify[r] = true))
-        })
+        }
 
         // domain specific
         // if we are publishing quotes, make sure to notify subscribers of orderbook also
         if (resource.endsWith('/quotes')) {
           const listingUrl = resource.substring(0, resource.length - '/quotes'.length)
-          Object.keys(resourceCache).forEach(resource => {
-            if (resource.startsWith(listingUrl)) {
-              resourcesToNotify[resource] = true
+          for (let resourceIndex in resourceCache) {
+            if (resourceCache.hasOwnProperty(resourceIndex) && resourceIndex.startsWith(listingUrl)) {
+              resourcesToNotify[resourceIndex] = true
             }
-          })
+          }
         }
 
         // domain specific
         // if we are publishing quotes, make sure to notify subscribers of orderbook also
         if (resource.endsWith('/orderbook')) {
           const listingUrl = resource.substring(0, resource.length - '/orderbook'.length)
-          Object.keys(resourceCache).forEach(resource => {
-            if (resource.startsWith(listingUrl)) {
-              resourcesToNotify[resource] = true
+          for (let resourceIndex in resourceCache) {
+            if (resourceCache.hasOwnProperty(resourceIndex) && resourceIndex.startsWith(listingUrl)) {
+              resourcesToNotify[resourceIndex] = true
             }
-          })
+          }
         }
 
         // always notify all subscriptions for original resource
@@ -223,17 +237,20 @@ export default function (token, endpoint) {
         const called = Object.create(null) // in lieu of Set
 
         // notify all subscriptions
-        Object.keys(resourcesToNotify).forEach(r => {
+        for (let r in resourcesToNotify) {
           const subscriptions = resourceToSubscription[r]
           if (subscriptions) {
-            subscriptions.forEach(subscription => {
-              if (!called[subscription.id]) {
-                queue[subscription.id] = () => subscription.callback(err, resourceCache[r], subscription.unsubscribeFn)
-                called[subscription.id] = true
+            for (let subscriptionIndex in subscriptions) {
+              if (subscriptions.hasOwnProperty(subscriptionIndex)) {
+                const subscription = subscriptions[subscriptionIndex]
+                if (!called[subscription.id]) {
+                  queue[subscription.id] = () => subscription.callback(err, resourceCache[r], subscription.unsubscribeFn)
+                  called[subscription.id] = true
+                }
               }
-            })
+            }
           }
-        })
+        }
 
         return
       },
